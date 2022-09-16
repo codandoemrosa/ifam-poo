fb05b23ab6543249ca665ffd58fa743ba118d3cc
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-16 17:37:27 -0400

    updated Triangulo.java

--- em-sala/src/Poligonos.java
+++ em-sala/src/Poligonos.java
@@ -1,12 +1,21 @@
 public class Poligonos {
     public static void main(String[] args) {
-        Triangulo t = new Triangulo();
+        Triangulo t1 = new Triangulo();
+        t1.mostrar();
+        System.out.println("-----------------------------");
+        System.out.println();
 
-        System.out.printf("Perímetro = %f\n", t.perimetro());
-        System.out.printf("Area = %f\n", t.area());
-        System.out.printf("Alfa = %f\n", t.anguloAlfa());
-        System.out.printf("Beta = %f\n", t.anguloBeta());
-        System.out.printf("Gama = %f\n", t.anguloGama());
+        Triangulo t2 = new Triangulo(8, 10, 7);
+        t2.mostrar();
+        System.out.println("-----------------------------");
+        System.out.println();
 
+        Triangulo t3 = new Triangulo(3.0, 4.0, 5.0);
+        t3.mostrar();
+        System.out.println("-----------------------------");
+        System.out.println();
+
+        double areaTotalTriangulos = t1.area() + t2.area() + t3.area();
+        System.out.println("Área total dos triângulos = " + areaTotalTriangulos);
     }
 }
