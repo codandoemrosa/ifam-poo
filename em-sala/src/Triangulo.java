032e7c06150155bb5eb5effad44e5e46da6c523b
Isadora Cerdeira <isadorascerdeira@gmail.com>
2022-09-16 17:36:44 -0400

    updated Triangulo.java

--- em-sala/src/Triangulo.java
+++ em-sala/src/Triangulo.java
@@ -2,9 +2,30 @@ import java.lang.Math;
 
 public class Triangulo {
 
-        int ladoA;
-        int ladoB;
-        int ladoC;
+        double ladoA;
+        double ladoB;
+        double ladoC;
+
+        Triangulo(){
+            System.out.println("Triângulo()");
+            ladoA = 3;
+            ladoB = 4;
+            ladoC = 5;
+        }
+
+        Triangulo(int a, int b, int c){
+            System.out.println("Triângulo(int)");
+            ladoA = a;
+            ladoB = b;
+            ladoC = c;
+        }
+
+        Triangulo(double a, double b, double c){
+            System.out.println("Triângulo(double)");
+            ladoA = a;
+            ladoB = b;
+            ladoC = c;
+        }
 
         public double area() {
             double p = perimetro() / 2;
@@ -31,5 +52,17 @@ public class Triangulo {
             double cosGama = (Math.pow(ladoA, 2) + Math.pow(ladoB, 2) - Math.pow(ladoC, 2)) / (2 * ladoA * ladoB);
             double gama = Math.acos(cosGama);
             return Math.toDegrees(gama);
+        }
+
+        public void mostrar(){
+
+            System.out.printf(" a = %f.2\n b = %f.2\n c = %f.2\n", ladoA, ladoB, ladoC);
+            System.out.println("\n---------- Cálculos ----------");
+
+            System.out.printf("Perímetro = %f\n", perimetro());
+            System.out.printf("Area = %f\n", area());
+            System.out.printf("Alfa = %f\n", anguloAlfa());
+            System.out.printf("Beta = %f\n", anguloBeta());
+            System.out.printf("Gama = %f\n", anguloGama());
         }
 }
